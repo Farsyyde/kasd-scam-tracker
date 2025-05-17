479dac75bd77300309b1e5f3ca39c98f4740740b
Farsyyde <dvathomas22@gmail.com>
2025-05-17 16:34:25 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ScamTracker from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScamTracker />
+  </React.StrictMode>
+);
